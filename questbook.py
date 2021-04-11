14cbf7e8baef294afa1ed3e4fe36b350ebee2921
wei-tsung-cheng <william.cheng@kkday.com>
2021-04-11 13:09:46 +0800

    ok

--- questbook.py
+++ questbook.py
@@ -49,10 +49,11 @@ class User(UserMixin, db.Model):  # 使用者資料表
     def __repr__(self):
         return f'name:{self.name},email:{self.email}'
 
+
+@app.route('/index.html')
 @app.route('/')
 def index():
-
-    return render_template("index.html")
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 80, debug=True)
