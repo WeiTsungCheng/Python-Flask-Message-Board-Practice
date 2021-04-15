f8ea70ad6d060cf7cc5f5ff69987f6b796dea3ea
wei-tsung-cheng <william.cheng@kkday.com>
2021-04-15 21:27:28 +0800

    oooo

--- guestbook.py
+++ guestbook.py
@@ -1,11 +1,16 @@
 from datetime import datetime
 from dateutil import tz
+from flask.helpers import url_for
+from flask.signals import request_finished
 from jinja2 import evalcontextfilter, Markup, escape
 import re
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import (UserMixin, LoginManager, login_manager,
+                        login_user, logout_user,
+                        login_required, current_user)
+from sqlalchemy.orm import eagerload
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -73,12 +78,66 @@ class User(UserMixin, db.Model):  # 使用者資料表
     def __repr__(self):
         return f'name:{self.name},email:{self.email}'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'admin'
+
+@login_manager.user_loader
+def load_user(user_id):
+    print(f"當前的 user id is {user_id}")
+    return User.query.get(int(user_id))
 
 @app.route('/index.html')
 @app.route('/')
 def index():
     gb = Guestbook.query.all()
     return render_template('index.html', books=gb)
+
+
+@app.route('/add_msg', methods=["POST"])
+def add_msg():
+    try:
+        guestname = request.form["guestname"]
+        email = request.form["email"]
+        message = request.form["message"]
+        icon = request.form["icon"]
+        gb = Guestbook(guestname=guestname, email=email,
+                        message=message, icon=icon)
+        db.session.add(gb)
+        db.session.commit()
+    except Exception as e:
+        print("出錯啦～無法新增留言！")
+        print(e)
+    return redirect("/")
+
+@app.route("/admin")
+def admin():
+    return render_template("login.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    email = request.form["email"]
+    pwd = request.form['password']
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        return "we can't find user"
+    elif not user.verify_password(pwd):
+        return "sry your password not correct"
+    elif not user.is_admin:
+        return "You are not manager"
+
+    login_user(user)
+    gb = Guestbook.query.all()
+
+    return render_template('list.html', books=gb, user=user)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 80, debug=True)
